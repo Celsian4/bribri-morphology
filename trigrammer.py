e6eb72cab2bb20680041344fe7785af8b7435e77
Celsian4 <carter.d.anderson26@dartmouth.edu>
2024-05-09 15:20:09 -0400

    Updated with the latest version of the trigrammer.py script and the latest version of the tokenized and hand-corrected Bribri data.

--- trigrammer.py
+++ trigrammer.py
@@ -1,11 +1,6 @@
-def word_pad(word: str, *, start_pad : str, end_pad : str) -> str:
-   '''
-   Pads the word with the specified pad character
-   @param word: the word to pad
-   @param start_pad: the starting pad character
-   @param end_pad: the ending pad character
-   @return the padded word
-   '''
-   return start_pad + word + end_pad
+def word_cutter(word : str, start_token : str, end_token : str) -> list[str]:
 
-print(word_pad("hello", start_pad = "<", end_pad = ">")) # <hello>
+   return [start_token] + word[::1] + [end_token]
+
+
+print(word_cutter("hello", "<", ">"))
